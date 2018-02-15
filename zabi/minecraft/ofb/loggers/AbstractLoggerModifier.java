d87888a8c8c4d4107ef63d686a5172977fd00b28
zabi94 <zabi94@gmail.com>
2018-02-15 15:09:43 +0100

    Headers

--- zabi/minecraft/ofb/loggers/AbstractLoggerModifier.java
+++ zabi/minecraft/ofb/loggers/AbstractLoggerModifier.java
@@ -1,3 +1,12 @@
+/**
+ * Class written by Zabi94
+ * Feb 15, 2018
+ * Distributed under MIT license
+ * 
+ * This class allows the installation of optifine via command line
+ * This is not affiliated with Optifine
+ */
+
 package zabi.minecraft.ofb.loggers;
 
 import java.io.PrintStream;
