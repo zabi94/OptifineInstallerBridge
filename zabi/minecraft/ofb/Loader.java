76524c94cacd7102239a3c02d84f19f5e478feb4
zabi94 <zabi94@gmail.com>
2018-02-15 14:39:38 +0100

    Logging options

--- zabi/minecraft/ofb/Loader.java
+++ zabi/minecraft/ofb/Loader.java
@@ -10,11 +10,17 @@
 package zabi.minecraft.ofb;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 public class Loader {
+	
+	private static boolean log = true;
 
 	@SuppressWarnings({ "rawtypes", "unchecked"})
 	public static void main(String[] args) {
@@ -23,14 +29,36 @@ public class Loader {
 		}
 		File destDir = new File(args[0]);
 		if (!destDir.exists()) {
-			throw new IllegalArgumentException("The specified directory doesn't exist");
+			throw new IllegalArgumentException("The specified directory doesn't exist: "+args[0]);
 		}
 		if (destDir.isFile()) {
 			throw new IllegalArgumentException("The specified directory is actually a file");
 		}
+		if (args.length>1 && args[1].equalsIgnoreCase("-nolog")) log=false;
 		File file = new File("ofb");
 		if (!file.exists() || file.isFile()) throw new IllegalStateException("No \"ofb\" directory found");
 		File[] files = file.listFiles(f -> f.exists() && f.isFile() && f.getName().toLowerCase().contains("optifine") && f.getName().toLowerCase().endsWith(".jar"));
+		File outputLog = new File("of_install_0.log");
+		int iteration = 1;
+		PrintStream stdout = System.out;
+		while (outputLog.exists()) {
+			outputLog = new File("of_install_"+iteration+".log");
+			iteration++;
+		}
+		PrintStream ps = null;
+		try {
+			outputLog.createNewFile();
+			OutputStream os = null;
+			if (log) os = new FileOutputStream(outputLog);
+			else os = new OutputStream() {
+				@Override
+				public void write(int b) throws IOException {
+				}
+			};
+			ps = new PrintStream(os, true);
+			System.setOut(ps);
+		} catch (IOException e1) {}
+		
 		boolean success = false;
 		for (File f:files) {
 			try {
@@ -38,6 +66,11 @@ public class Loader {
 				Class classToLoad = Class.forName("optifine.Installer", true, child);
 				Method method = classToLoad.getDeclaredMethod("doInstall", File.class);
 				method.invoke(null, destDir);
+				System.setOut(stdout);
+				if (ps!=null) {
+					ps.flush();
+					ps.close();
+				}
 				success=true;
 				System.out.println("Optifine installation successful");
 				System.exit(0);
